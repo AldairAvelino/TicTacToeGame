a1f24731c691db805d36c52426c94444de6e1e36
Aldair Avelino <aldair03avelino@gmail.com>
2024-04-15 23:30:04 +0100

    feat: Handle invalid user input exception - AA

--- src/tictactoegame/GameBoard.java
+++ src/tictactoegame/GameBoard.java
@@ -2,11 +2,16 @@ package tictactoegame;
 
 import java.util.Scanner;
 
-public class GameBoard {
-    private static char[][] board = new char[3][3];
-    private static char currentPlayer;
 
-    public static void initializeBoard() {
+public class GameBoard {
+    private char[][] board;
+
+    public GameBoard() {
+        board = new char[3][3];
+        initializeBoard();
+    }
+
+    private void initializeBoard() {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 board[i][j] = '-';
@@ -14,7 +19,7 @@ public class GameBoard {
         }
     }
 
-    public static void printBoard() {
+    public void printBoard() {
         System.out.println("-------------");
         for (int i = 0; i < 3; i++) {
             System.out.print("| ");
@@ -26,26 +31,26 @@ public class GameBoard {
         }
     }
 
-    public static boolean isValidMove(int position) {
+    public boolean isValidMove(int position) {
         if (position < 1 || position > 9)
             return false;
-        
+
         int row = (position - 1) / 3;
         int col = (position - 1) % 3;
         return board[row][col] == '-';
     }
 
-    public static void makeMove(char player, int position) {
+    public void makeMove(char playerSymbol, int position) {
         int row = (position - 1) / 3;
         int col = (position - 1) % 3;
-        board[row][col] = player;
+        board[row][col] = playerSymbol;
     }
 
-    public static boolean checkWin() {
+    public boolean checkWin() {
         return checkRows() || checkColumns() || checkDiagonals();
     }
 
-    public static boolean checkRows() {
+    private boolean checkRows() {
         for (int i = 0; i < 3; i++) {
             if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != '-') {
                 return true;
@@ -54,7 +59,7 @@ public class GameBoard {
         return false;
     }
 
-    public static boolean checkColumns() {
+    private boolean checkColumns() {
         for (int j = 0; j < 3; j++) {
             if (board[0][j] == board[1][j] && board[1][j] == board[2][j] && board[0][j] != '-') {
                 return true;
@@ -63,12 +68,12 @@ public class GameBoard {
         return false;
     }
 
-    public static boolean checkDiagonals() {
+    private boolean checkDiagonals() {
         return (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != '-') ||
-               (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != '-');
+                (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != '-');
     }
 
-    public static boolean checkDraw() {
+    public boolean checkDraw() {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 if (board[i][j] == '-') {
@@ -79,11 +84,11 @@ public class GameBoard {
         return true;
     }
 
-    public static void resetGame() {
+    public void resetBoard() {
         initializeBoard();
     }
-
-    public static boolean continueGame(Scanner scanner, char winner) {
+    
+    public static boolean continueGame(Scanner scanner, char currentPlayerSymbol) {
         System.out.println("Do you want to continue playing? (yes/no)");
         String playAgain = scanner.next();
         if (playAgain.equalsIgnoreCase("yes")) {
@@ -93,4 +98,5 @@ public class GameBoard {
             return false;
         }
     }
+
 }
